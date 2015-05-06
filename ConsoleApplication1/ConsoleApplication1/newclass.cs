98c2e9e571015a3c33f0e921425de82fb07404c0
Mark Morley 2 <someaddress@mark-morley.co.uk>
2015-05-06 20:35:24 +0100

    added new class from azure

---
+++ ConsoleApplication1/ConsoleApplication1/newclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class newclass
+    {
+        public newclass()
+        {
+            // added class to main branch from azure
+        }
+    }
+}
